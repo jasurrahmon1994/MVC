5a8c687a67b1e454f513731339177097c876f50d
jasurrahmon24 <jasurrahmon24@gmail.com>
2022-10-06 09:05:13 +0500

    Working with @ModelAttribute

--- src/main/java/uz/jasur/controllers/PeopleController.java
+++ src/main/java/uz/jasur/controllers/PeopleController.java
@@ -2,10 +2,9 @@ package uz.jasur.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import uz.jasur.dao.PersonDAO;
+import uz.jasur.models.Person;
 
 @Controller
 @RequestMapping("/people")
@@ -21,9 +20,21 @@ public class PeopleController {
      model.addAttribute("people", personDAO.index());
         return "/people/index";
     }
+
     @GetMapping("/{id}")
     public String show(@PathVariable("id") int id, Model model){
         model.addAttribute("person", personDAO.show(id));
         return "/people/show";
+    }
+
+    @GetMapping("/new")
+    public String fillForm(@ModelAttribute("person") Person person){
+        return "people/new";
+    }
+
+    @PostMapping()
+    public String create(@ModelAttribute("person") Person person){
+        personDAO.save(person);
+        return "redirect:/people";
     }
 }
