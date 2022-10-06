712c52f9e6e4aeb46445bd9b93b77aa3b25ce2f7
jasurrahmon24 <jasurrahmon24@gmail.com>
2022-10-06 11:58:42 +0500

    Bug: Patch & Delete operation

--- src/main/java/uz/jasur/config/ServletConfig.java
+++ src/main/java/uz/jasur/config/ServletConfig.java
@@ -1,6 +1,10 @@
 package uz.jasur.config;
 
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
 
 
 public class ServletConfig extends AbstractAnnotationConfigDispatcherServletInitializer {
@@ -19,4 +23,15 @@ public class ServletConfig extends AbstractAnnotationConfigDispatcherServletInit
     protected String[] getServletMappings() {
         return new String[] {"/"};
     }
+
+//    @Override
+//    public void onStartup(ServletContext aServletContext) throws ServletException {
+//        super.onStartup(aServletContext);
+//        registerHiddenFieldFilter(aServletContext);
+//    }
+//
+//    private void registerHiddenFieldFilter(ServletContext aContext) {
+//        aContext.addFilter("hiddenHttpMethodFilter",
+//                new HiddenHttpMethodFilter()).addMappingForUrlPatterns(null ,true, "/*");
+//    }
 }
--- src/main/java/uz/jasur/config/SpringConfig.java
+++ src/main/java/uz/jasur/config/SpringConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.thymeleaf.spring5.SpringTemplateEngine;
@@ -13,6 +14,7 @@ import org.thymeleaf.spring5.view.ThymeleafViewResolver;
 
 @Configuration
 @ComponentScan("uz.jasur")
+@PropertySource("classpath:application.properties")
 @EnableWebMvc
 public class SpringConfig {
     private final ApplicationContext applicationContext;
@@ -46,3 +48,4 @@ public class SpringConfig {
         registry.viewResolver(resolver);
     }
 }
+
